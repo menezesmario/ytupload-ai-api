6557823d2f37fa9a3a64577d449dba2ec6d03c03
Mário Menezes <mario082@gmail.com>
2023-09-14 17:08:19 -0300

    create ai completion route finished

---
+++ src/routes/create-ai-completion.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from "fastify";
+import {z} from 'zod'
+import { prisma } from "../lib/prisma";
+import { createReadStream } from "node:fs";
+import { openai } from "../lib/openai";
+
+
+export async function generateAICompletionRoute(app: FastifyInstance){
+    app.post('/ai/complete', async (req) => {
+        const bodySchema = z.object({
+            videoId: z.string().uuid(),
+            template: z.string(),
+            temperature: z.number().min(0).max(1).default(0.5),
+        })
+
+        const { videoId, template, temperature } = bodySchema.parse(req.body)
+
+        return {
+            videoId,
+            template,
+            temperature,
+        }        
+    })
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import {fastify} from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/create-ai-completion";
 
 
 const app = fastify()
@@ -9,6 +10,7 @@ const app = fastify()
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
     port: 3333,
