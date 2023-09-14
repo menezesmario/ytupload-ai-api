716afb1ec8d0385c81274c26534ec47dd0ba0ba0
Mário Menezes <mario082@gmail.com>
2023-09-14 17:33:15 -0300

    created route complete for transcription

--- src/routes/create-ai-completion.ts
+++ src/routes/create-ai-completion.ts
@@ -6,7 +6,7 @@ import { openai } from "../lib/openai";
 
 
 export async function generateAICompletionRoute(app: FastifyInstance){
-    app.post('/ai/complete', async (req) => {
+    app.post('/ai/complete', async (req, res) => {
         const bodySchema = z.object({
             videoId: z.string().uuid(),
             template: z.string(),
@@ -15,10 +15,26 @@ export async function generateAICompletionRoute(app: FastifyInstance){
 
         const { videoId, template, temperature } = bodySchema.parse(req.body)
 
-        return {
-            videoId,
-            template,
+        const video = await prisma.video.findFirstOrThrow({
+            where: {
+                id: videoId,
+            }
+        })
+
+        if(!video.transcription) {
+            return res.status(400).send({ error: 'Video transcription was not generated yet`' })
+        }
+
+        const promptMessage = template.replace('{transcription}', video.transcription)
+
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo-16k',
             temperature,
-        }        
+            messages: [ {
+                role: 'user', content: promptMessage,
+            }]
+        })
+
+        return response
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import {fastify} from "fastify";
+import { fastifyCors } from '@fastify/cors'
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
@@ -6,6 +7,10 @@ import { generateAICompletionRoute } from "./routes/create-ai-completion";
 
 
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*',
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
